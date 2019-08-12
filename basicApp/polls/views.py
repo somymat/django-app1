444b1b3d58f6d39b2ff5ce153823cf643c3eef17
ravisarath <ravisarath64@gmail.com>
2019-08-12 16:07:25 +0530

    test pages

--- basicApp/polls/views.py
+++ basicApp/polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponce
+
 
 # Create your views here.
+def detail(request, question_id):((
+    return HttpResponce("you are looking at the questions {}", .format("question_id"))
+def results(request, question_id):
+    return HttpResponce("you are looking at the result of the question {}",.format("question_id"))
+def vote(request, question_id):
+    return HttpResponce("you are voting on question {}",.format("question_id"))
