e6e7d33126d5fb543e8800f6174b2bfd095f3995
ShilpaMerinMathew <shilpamerin246@gmail.com>
2019-08-13 13:12:18 +0530

    mongo setup

--- basicApp/basicApp/settings.py
+++ basicApp/basicApp/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import mongoengine
+import django_mongodb_engine
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +76,22 @@ WSGI_APPLICATION = 'basicApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django_mongodb_engine',
+            'ENFORCE_SCHEMA': False,
+            'NAME': 'mydb',
+            'HOST': '127.0.0.1:27017',
+            'PORT': 27017,
+        }
     }
-}
 
 
 # Password validation
--- basicApp/polls/models.py
+++ basicApp/polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from basicApp.settings import DATABASES
+from mongoengine import *
+import datetime
+connect(DBNAME)
 # Create your models here.
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
