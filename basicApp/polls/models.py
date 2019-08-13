b83ac2ecfca5123fe888224bc01f8d95174c3651
ShilpaMerinMathew <shilpamerin246@gmail.com>
2019-08-13 16:01:49 +0530

    django-mongo setup  with djongo

--- basicApp/polls/models.py
+++ basicApp/polls/models.py
@@ -5,17 +5,15 @@ import datetime
 
 # Create your models here.
 class Question(models.Model):
-    # ...
     def __str__(self):
         return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
-    # ...
     def __str__(self):
         return self.choice_text
-
-
-class Question(models.Model):
-    # ...
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,null=True)
+    choice_text = models.CharField(max_length=200,null=True)
+    votes = models.IntegerField(default=0)
