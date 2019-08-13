e7b774053469843c242ccca05202437dfeb1bfce
ShilpaMerinMathew <shilpamerin246@gmail.com>
2019-08-13 17:22:01 +0530

    registering app to site

--- basicApp/polls/models.py
+++ basicApp/polls/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from djongo import models
 from basicApp.settings import DATABASES
+from django.utils import timezone
 import datetime
 
 # Create your models here.
 class Question(models.Model):
+    question_text = models.CharField(max_length=200,null=True)
+    pub_date = models.DateTimeField('date published',null=True)
     def __str__(self):
         return self.question_text
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 
 class Choice(models.Model):
-    def __str__(self):
-        return self.choice_text
     question = models.ForeignKey(Question, on_delete=models.CASCADE,null=True)
     choice_text = models.CharField(max_length=200,null=True)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
